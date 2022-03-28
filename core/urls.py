dbe006ff684a61bf06d1f1fb1ba45a0ae9c0a9d1
Alexander Opryshko <alexopryshko@yandex.ru>
2022-03-28 10:06:39 +0300

    lesson33

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 
 from core import views
 
-urlpatterns = [
-    path("signup", views.SignupView.as_view()),
-    # path("signin", views.ChapterSwapView.as_view()),
-]
+urlpatterns = []
