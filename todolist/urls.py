ad4b2047c53b0455494d245b3c86912fccdd535d
sergeymalenko <malenko@ktsstudio.ru>
2022-04-18 11:22:11 +0300

    hide bot application

--- todolist/urls.py
+++ todolist/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path("oauth/", include("social_django.urls", namespace="social")),
     path("core/", include("core.urls")),
     path("goals/", include("goals.urls")),
-    path("bot/", include("bot.urls")),
+    # path("bot/", include("bot.urls")),
     re_path(
         r"^swagger/$",
         schema_view.with_ui("swagger", cache_timeout=0),
